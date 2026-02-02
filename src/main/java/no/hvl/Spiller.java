16a85a0a6f83a9c14fd36a5e3ba6cf6e5bc0d4a8
tjommilaskus <mps.94@me.com>
2026-02-02 14:34:05 +0100

    La til getters og setters

--- src/main/java/no/hvl/Spiller.java
+++ src/main/java/no/hvl/Spiller.java
@@ -1,3 +1,20 @@
+package no.hvl;
+/*
+* Dette er Spiler Klassen
+*  @Author Martin Palm
+ */
 public class Spiller {
     private String navn;
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public Spiller(String navn) {
+        this.navn = navn;
+    }
 }
