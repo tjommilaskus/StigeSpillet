33744139987603f70101763ef432f49e9008fb68
Ebbehj <168018590+Ebbehj@users.noreply.github.com>
2026-02-05 11:48:46 +0100

    Update Terning.java

--- src/main/java/no/hvl/Terning.java
+++ src/main/java/no/hvl/Terning.java
@@ -1,12 +1,15 @@
 package no.hvl;
 
 /**
- * Terning klasse retuner, ved funksjonen tril random 1-6
+ * Terning
  *  @Author Magnus Fomdenes
  */
 public class Terning {
     private int verdi;
 
+    /**
+     * Terning metode som retuner random int fra 1-6 og Printer ut resultatet av kastet
+     */
     int trill(){
         int i = (int) (Math.floor(Math.random() * 6) + 1);
         System.out.println("Du rullet\uD83C\uDFB2: " + i);
